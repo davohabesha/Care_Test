c67a3378f803b060fd94ad35c76630af882a7611
davohabesha <davohabesha@gmail.com>
2020-04-29 13:49:10 -0400

    care_first project

---
+++ src/test/java/project/BaseClass.java
@@ -0,0 +1,42 @@
+package project;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class BaseClass {
+	public static WebDriver driver;
+	public static String url= "https://individual.carefirst.com/individuals-families/home.page#";
+	
+	
+	
+	@BeforeSuite
+
+public void setup ( ) {
+	
+	 System.setProperty("webdriver.chrome.driver", "/Users/mistreaderaw/Downloads/chromedriver");
+	driver= new ChromeDriver();
+	driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+	driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+	driver.manage().deleteAllCookies();
+	driver.manage().window().maximize();
+	driver.get(url);
+	//
+}
+
+
+ @AfterSuite
+ 
+ public void theardown() { 
+	 driver.quit(); 
+  }
+ 
+ 
+}
---
+++ src/test/java/project/TC_CAREFIR_001.java
@@ -0,0 +1,69 @@
+package project;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import static org.testng.Assert.assertEquals;
+
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC_CAREFIR_001 extends BaseClass {
+	
+	@Test (priority= 1)
+	public void varifytitle() {
+	String titleofthepage= driver.getTitle();
+	String expectedtitle= "CareFirst | Medical, Dental and Vision Insurance in Maryland, the District of Columbia & Northern Virginia";
+	AssertJUnit.assertEquals(titleofthepage , expectedtitle );
+	System.out.println( titleofthepage );
+}
+	
+	@Test(priority=2)	
+	public void closechildwindow() {
+		String currentwindow= driver.getWindowHandle();
+		System.out.println(currentwindow);
+		Set<String> windows= driver.getWindowHandles();
+		System.out.println(windows);
+		driver.switchTo().window(currentwindow);
+	   driver.findElement(By.xpath("//button[@class='close']")).click();
+	}
+	   
+	   @Test (priority=3)
+	   
+	   public void varifylink( ) {
+		WebElement indfamilik= driver.findElement(By.xpath("//*[contains(text(),'Coverage')]"));
+		
+		indfamilik.click();
+		
+	}
+	@Test (priority=4)
+	public void clickonfamilink (){
+		
+		driver.findElement(By.xpath("//a[@title='Individual & Family Plans' ] " )).click();
+		String title= driver.getTitle();
+		String url= driver.getCurrentUrl();
+		System.out.println(title +  " " + url);
+	}
+@Test(priority=5)
+public void varifyquotebtn() {
+		
+        
+	
+	     WebElement quotebtn= driver.findElement(By.xpath("//*[contains(text(),'Get a Quote')]"));
+		 JavascriptExecutor js =(JavascriptExecutor)driver;
+         js.executeScript("arguments[0].scrollIntoView();",quotebtn);
+         quotebtn.click();
+		String qutitle= driver.getTitle();
+		String quurl= driver.getCurrentUrl();
+		System.out.println(qutitle +  " " + quurl); }
+	
+}
